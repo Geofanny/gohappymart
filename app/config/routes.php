f2a912ed23c25c32e47b1c22b76957737eaec82c
doff23 <daffashadqi23@gmail.com>
2025-10-20 03:54:09 +0700

    ✅ Selesai tugas 3: CRUD Admin Produk (tambah, edit, hapus, lihat produk) sudah berfungsi penuh dan siap review

--- app/config/routes.php
+++ app/config/routes.php
@@ -8,5 +8,11 @@ return [
     'keranjang' => 'KeranjangController',
     'tentang' => 'TentangController',
     'akun'      => 'AkunController',
-    'akun/update' => 'AkunController@update',
+    'admin-produk'              => 'AdminProductController',
+    'admin-produk/create'       => 'AdminProductController@create',
+    'admin-produk/store'        => 'AdminProductController@store',
+    'admin-produk/edit'         => 'AdminProductController@edit',
+    'admin-produk/update'       => 'AdminProductController@update',
+    'admin-produk/delete'       => 'AdminProductController@delete',
+    'admin-produk/show'         => 'AdminProductController@show',
 ];
